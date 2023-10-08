9b0c56711a163c700ac0258eb96a870b3a9b9a9a
3noud <anoudbin@gmail.com>
2023-10-08 10:11:49 +0300

    Initial Commit

---
+++ presentYourself/presentYourselfApp.swift
@@ -0,0 +1,17 @@
+//
+//  presentYourselfApp.swift
+//  presentYourself
+//
+//  Created by AlAnoud on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct presentYourselfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
