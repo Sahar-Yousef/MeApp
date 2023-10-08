4170724e5c40185f2e8bab85728ab08b58def473
Sahar-Yousef <Sell.lla.78@gmail.com>
2023-10-08 10:03:06 +0300

    Initial Commit

---
+++ MeApp/MeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeAppApp.swift
+//  MeApp
+//
+//  Created by Sahora on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
